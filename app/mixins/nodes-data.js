738aa108b9c61e9b4d53b97a9a19a43ec09f1466
Justaphf <juholmes@vt.edu>
2018-07-26 22:05:13 -0400

    Correct service bit mapping for CASH and GRAPHENE

--- app/mixins/nodes-data.js
+++ app/mixins/nodes-data.js
@@ -21,8 +21,8 @@ export default Mixin.create({
     if (services & 4) { serviceBits.push('NODE_BLOOM'); }
     if (services & 8) { serviceBits.push('NODE_WITNESS'); }
     if (services & 16) { serviceBits.push('NODE_XTHIN'); }
-    if (services & 16) { serviceBits.push('NODE_XTHIN'); }
-    if (services & 32) { serviceBits.push('NODE_GRAPHENE'); }
+    if (services & 32) { serviceBits.push('NODE_CASH'); }
+    if (services & 64) { serviceBits.push('NODE_GRAPHENE'); }
     return serviceBits;
   },
 
