943d0e4fd8484a2fc9e587e5f60fabc676567e41
Axel Gembe <derago@gmail.com>
2020-09-22 11:59:25 +0700

    Update for November 2020 rules
    This updates the nodesByUserAgentInConsensus function for the
    November 2020 upgrade. This should be close to the coin.dance
    numbers.
    
    This removes the bchd version check (which was wrong, 'bchd' should
    be lowercase) because there is no released version with support
    for ASERT yet.
    
    This also removes counting for unknown nodeTypes. It is wrong to
    count clients like Bitcoin XT to be in consensus.

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,1 +1,6 @@
-export default { BUminVer: '1.8.0', ABCminVer: '0.21.0', BCHDminVer: '0.16.0'};
+export default {
+  BUminVer: '1.9.0',
+  ABCminVer: '0.22.0',
+  BCHNminVer: '22.0.0',
+  FloweeminVer: '1.0.0',
+};
